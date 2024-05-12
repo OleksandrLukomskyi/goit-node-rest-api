2e5e2d80cb1491555673f6faa5de3aa1cbe10de2
OleksandrLukomskyi <121460896+OleksandrLukomskyi@users.noreply.github.com>
2024-05-12 23:14:53 +0200

    add

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
 import contactsRouter from './routes/contactsRouter.js';
-
+import './db.js';
 const app = express();
 
 app.use(morgan('tiny'));
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use('/api/contacts', contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,46 +1,79 @@
 import HttpError from '../helpers/HttpError.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
-import contactsService from '../services/contactsServices.js';
+import Contacts from '../models/contacts.js';
 
 export const getAllContacts = ctrlWrapper(async (req, res) => {
-  const contacts = await contactsService.listContacts();
-  return res.status(200).json(contacts);
+  const contacts = await Contacts.find();
+  res.send(contacts);
 });
 
 export const getOneContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.getContactById(id);
+  const contact = await Contacts.findById(id);
   if (!contact) {
     throw HttpError(404);
   }
-  return res.status(200).json(contact);
+  res.send(contact);
 });
 
 export const deleteContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.removeContact(id, req.body);
+  const contact = await Contacts.findByIdAndDelete(id);
   if (!contact) {
     throw HttpError(404);
   }
-  return res.status(200).json(contact);
+  res.send(contact);
 });
 
 export const createContact = ctrlWrapper(async (req, res) => {
-  const contact = await contactsService.addContact(req.body);
-  return res.status(201).json(contact);
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+  const result = await Contacts.create(contact);
+  res.status(201).send(result);
 });
 
 export const updateContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.updateContactById(id, req.body);
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite: req.body.favorite,
+  };
+  const result = await Contacts.findByIdAndUpdate(id, contact);
   if (!contact) {
     throw HttpError(404);
   }
-  if (Object.keys(req.body).length === 0) {
+  if (Object.keys(contact).length === 0) {
     return res
       .status(400)
       .json({ message: 'Body must have at least on field' });
   }
 
-  return res.status(200).json(contact);
+  res.send(result);
+});
+
+export const updeteStatusContact = ctrlWrapper(async (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite: req.body.favorite,
+  };
+  const result = await Contacts.findByIdAndUpdate(id, contact);
+  if (!contact) {
+    throw HttpError(404);
+  }
+  if (Object.keys(contact).length === 0) {
+    return res
+      .status(400)
+      .json({ message: 'Body must have at least on field' });
+  }
+
+  res.send(result);
 });
